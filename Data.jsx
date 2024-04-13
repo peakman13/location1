658b879d1aabc8b82ecfc657bfd880d7817a2d96
Cihan KIZILTAŞ <161931133+peakman13@users.noreply.github.com>
2024-04-13 13:47:22 +0300

    Data.jsx

---
+++ Data.jsx
@@ -0,0 +1,51 @@
+import Paris from './images/P.jpeg';
+import Amsterdam  from './images/ams.jpg';
+import Roma  from './images/r.jpeg';
+import Berlin from './images/b.jpeg';
+
+
+ export const courses = [
+    {
+    id : 1234,
+    title : "effiel tower ",
+    price : 10,
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae? Nemo, quam?",
+    link : "https://github.com/peakman13",
+    image : Paris
+
+    },
+
+    {
+        id : 2345,
+        title : "dom square ",
+        price : 20,
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae? Nemo, quam?",
+        link : "https://github.com/peakman13",
+        image : Amsterdam
+    
+    },
+
+
+    {
+    id : 3456,
+    title : "collesium ",
+    price : 30,
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae? Nemo, quam?",
+    link : "https://github.com/peakman13",
+    image : Roma
+
+    },
+
+    {
+        id : 4567,
+        title : "brendeburgh gate ",
+        price : 40,
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae? Nemo, quam?",
+        link : "https://github.com/peakman13",
+        image : Berlin
+    
+    }
+
+
+
+];
